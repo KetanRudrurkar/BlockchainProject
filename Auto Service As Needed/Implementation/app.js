88bf57cdf5c1be6b40d398471cacecf5c2c1aa3b
vedant-bhoj <bhoj.vedant28@gmail.com>
2020-04-11 00:37:20 -0700

    EJS updated

--- Auto Service As Needed/Implementation/app.js
+++ Auto Service As Needed/Implementation/app.js
@@ -4,6 +4,8 @@ var bodyparser = require("body-parser");
 var mongoose = require("mongoose");
 var Car = require("./models/cardb");
 var seed =  require("./seeds")
+var path = require("path");
+app.use(express.static(__dirname + "/etc")); // using CSS and images from public directory
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost/carapp", { useNewUrlParser: true });
 
@@ -11,9 +13,18 @@ seed();
 
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req,res)=>{
-    res.send("This is the home page.");
+  
+    Car.find(function(err, cardata){
+        if(err){
+            console.log(err);
+        }else{
+            data = JSON.stringify(cardata);
+            res.render("index", {cardata:data});
+        }
+    })
 })
 
 function calculateenginehealth(){
