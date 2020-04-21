b9b624acb05dc28097b8c88a18bbe18d95c03a59
vedant-bhoj <bhoj.vedant28@gmail.com>
2020-04-21 12:16:36 -0700

    graphs changed

--- client/src/dashboard.js
+++ client/src/dashboard.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { Bar, Line, Pie, Radar, Doughnut, Polar, HorizontalBar } from 'react-chartjs-2';
 import { faGreaterThanEqual } from '@fortawesome/free-solid-svg-icons';
+import './dashboard.css';
 
 class Dashboard extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            CarDetailsApi:{
-                labels:[],
-                datasets:[
+        this.state = {
+            CarDetailsApi: {
+                labels: [],
+                datasets: [
                     {
-                        label:'Car Details',
-                        data:[
-                               
+                        label: 'Car Details',
+                        data: [
+
                         ],
-                        backgroundColor:[
+                        backgroundColor: [
                             'rgba(255,99,132,0.6)',
                             'rgba(54,162,235,0.6)',
                             'rgba(255,206,86,0.6)',
@@ -23,19 +24,19 @@ class Dashboard extends Component {
                             'rgba(255,159,64,0.6)',
                             'rgba(255,99,132,0.6)'
                         ]
-                        
+
                     }
                 ]
             },
-            MilesApi:{
-                labels:[],
-                datasets:[
+            MilesApi: {
+                labels: [],
+                datasets: [
                     {
-                        label:'Miles usage since last service',
-                        data:[
-                               
+                        label: 'Miles usage since last service',
+                        data: [
+
                         ],
-                        backgroundColor:[
+                        backgroundColor: [
                             'rgba(255,99,132,0.6)',
                             'rgba(54,162,235,0.6)',
                             'rgba(255,206,86,0.6)',
@@ -48,19 +49,19 @@ class Dashboard extends Component {
                             'rgba(255,69,112,0.6)',
                             'rgba(255,59,100,0.6)'
                         ]
-                        
+
                     }
                 ]
             },
-            DaysApi:{
-                labels:[],
-                datasets:[
+            DaysApi: {
+                labels: [],
+                datasets: [
                     {
-                        label:'Days since last service',
-                        data:[
-                               
+                        label: 'Days since last service',
+                        data: [
+
                         ],
-                        backgroundColor:[
+                        backgroundColor: [
                             'rgba(255,99,132,0.6)',
                             'rgba(54,162,235,0.6)',
                             'rgba(255,206,86,0.6)',
@@ -73,141 +74,154 @@ class Dashboard extends Component {
                             'rgba(255,69,112,0.6)',
                             'rgba(255,59,100,0.6)'
                         ]
-                        
+
                     }
                 ]
             },
             show: false, message: ""
-            
-         }
-         this.retrivepartsinfo = this.retrivepartsinfo.bind(this);
+
+        }
+        this.retrivepartsinfo = this.retrivepartsinfo.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.clear = this.clear.bind(this);
     }
-    handleChange(num){
+    handleChange(num) {
         this.setState({
             Vin: num.target.value
         });
     }
-    retrivepartsinfo(val){
+    retrivepartsinfo(val) {
         fetch('/calculatecarhealth', {
             method: 'post',
             headers: new Headers({
                 'Content-Type': 'application/json',
             }),
-            body: JSON.stringify({id: this.state.Vin})
+            body: JSON.stringify({ id: this.state.Vin })
         })
-        .then(resp => {
-            if(!resp.ok){
-                if(resp.status >= 400 && resp.status <500){
-                    return resp.json().then(data =>{
-                        let err = {errorMessage : data.message}
+            .then(resp => {
+                if (!resp.ok) {
+                    if (resp.status >= 400 && resp.status < 500) {
+                        return resp.json().then(data => {
+                            let err = { errorMessage: data.message }
+                            throw err;
+                        })
+                    } else {
+                        let err = { errorMessage: 'Server errror!' };
                         throw err;
-                    })
-                } else{
-                    let err = {errorMessage: 'Server errror!'};
-                    throw err;
+                    }
                 }
-            }
-            return resp.json();
-        })
-        .then(receivedobject =>{
-            if(receivedobject.message){
-            this.setState({message: receivedobject.message, show: false});
-            }else{
-                // this.setState({partsneedservice: [], partsdontneedservice: []})
-                const updatedCarDetailsApi = {...this.state.CarDetailsApi}
-                updatedCarDetailsApi.labels = receivedobject.singleData.carDetailsLabels
-                updatedCarDetailsApi.datasets[0].data = receivedobject.singleData.CarDetailsValues
-                const updatedMilesApi = {...this.state.MilesApi}
-                updatedMilesApi.labels = receivedobject.singleData.MilesLabels
-                updatedMilesApi.datasets[0].data = receivedobject.singleData.MilesValues
-                const updatedDaysApi = {...this.state.DaysApi}
-                updatedDaysApi.labels = receivedobject.singleData.DaysLabels
-                updatedDaysApi.datasets[0].data = receivedobject.singleData.DaysValues
-                this.setState({CarDetailsApi: updatedCarDetailsApi , MilesApi: updatedMilesApi , DaysApi: updatedDaysApi , show: true})
+                return resp.json();
+            })
+            .then(receivedobject => {
+                if (receivedobject.message) {
+                    this.setState({ message: receivedobject.message, show: false });
+                } else {
+                    // this.setState({partsneedservice: [], partsdontneedservice: []})
+                    const updatedCarDetailsApi = { ...this.state.CarDetailsApi }
+                    updatedCarDetailsApi.labels = receivedobject.singleData.carDetailsLabels
+                    updatedCarDetailsApi.datasets[0].data = receivedobject.singleData.CarDetailsValues
+                    const updatedMilesApi = { ...this.state.MilesApi }
+                    updatedMilesApi.labels = receivedobject.singleData.MilesLabels
+                    updatedMilesApi.datasets[0].data = receivedobject.singleData.MilesValues
+                    const updatedDaysApi = { ...this.state.DaysApi }
+                    updatedDaysApi.labels = receivedobject.singleData.DaysLabels
+                    updatedDaysApi.datasets[0].data = receivedobject.singleData.DaysValues
+                    this.setState({ CarDetailsApi: updatedCarDetailsApi, MilesApi: updatedMilesApi, DaysApi: updatedDaysApi, show: true })
 
-                // this.setState({carDetails: receivedobject.carDetails, show: true}) 
-            }
-        })
+                    // this.setState({carDetails: receivedobject.carDetails, show: true}) 
+                }
+            })
     }
-    clear(){
-        this.setState({show: false, message: ""})
+    clear() {
+        this.setState({ show: false, message: "" })
     }
     static defaultProps = {
-        displayTitle:true,
+        displayTitle: true,
         displayLegend: true,
-        legendPosition:'right',
-        location:'City'
-      }
-    render() { 
+        legendPosition: 'right',
+        location: 'City'
+    }
+    render() {
         const displaymessage = this.state.message;
         return (
             <div>
-            Enter Car VIN: <input type="text"  id="id" value={this.state.Vin} onChange={this.handleChange}/>
-            <button onClick={this.retrivepartsinfo}>Show Analytics</button>
-            {/* <button onClick={this.clear}>Clear</button> */}
-            {this.state.message ==""?  (<div style={{backgroundColor:"whitesmoke"}}>
-        
-            <div className="MilesGraph">
+                Enter Car VIN: <input type="text" id="id" value={this.state.Vin} onChange={this.handleChange} />
+                <button onClick={this.retrivepartsinfo}>Show Analytics</button>
+                {/* <button onClick={this.clear}>Clear</button> */}
+                {this.state.message == "" ? (<div class="flex-container1">
 
-            <Pie
-      data={this.state.MilesApi}
-      options={{
-        title:{
-          display:this.props.displayTitle,
-          text: 'Miles Usage Since Last Service',
-          fontSize:40
-        },
-        legend:{
-          display:this.props.displayLegend,
-          position:this.props.legendPosition
-        }
-      }}
-    />
-            </div>
-            <div className="DaysGraph">
-            <HorizontalBar 
-  data={this.state.DaysApi}
-  options={{
-    title:{
-      display:this.props.displayTitle,
-      text:'Days Since Last service',
-      fontSize:40
-    },
-    legend:{
-      display:this.props.displayLegend,
-      position:this.props.legendPosition
-    }
-  }}
-/>
-</div>
+                    <div className="MilesGraph">
 
-<div className="CarDetailsGraph">
-            <Radar
-            data={this.state.CarDetailsApi}
-            options={{
-                title:{
-                  display:this.props.displayTitle,
-                  text:'Car Details',
-                  fontSize:40
-                },
-                legend:{
-                  display:this.props.displayLegend,
-                  position:this.props.legendPosition
-                }
-              }}
-            />
-            </div>
-        </div>) : (<div>
+                        <Pie
+                            data={this.state.MilesApi}
+                            options={{
+                                title: {
+                                    display: this.props.displayTitle,
+                                    text: 'Miles Usage Since Last Service',
+                                    fontSize: 20
+                                },
+                                legend: {
+                                    display: this.props.displayLegend,
+                                    position: this.props.legendPosition
+                                },
+                                responsive: true,
+                                maintainAspectRatio: false
+                            }}
+                        />
+                    </div>
+                    <div className="DaysGraph">
+                        <HorizontalBar
+                            data={this.state.DaysApi}
+                            options={{
+                                title: {
+                                    display: this.props.displayTitle,
+                                    text: 'Days Since Last service',
+                                    fontSize: 20
+                                },
+                                legend: {
+                                    display: this.props.displayLegend,
+                                    position: this.props.legendPosition
+                                },
+                                responsive: true,
+                                maintainAspectRatio: false
+                            }}
+                        />
+                    </div>
+
+                    <div className="CarDetailsGraph">
+                        <Radar
+                            data={this.state.CarDetailsApi}
+                            options={{
+                                title: {
+                                    display: this.props.displayTitle,
+                                    text: 'Car Details',
+                                    fontSize: 20
+                                },
+                                legend: {
+                                    display: this.props.displayLegend,
+                                    position: this.props.legendPosition
+                                },
+                                layout: {
+                                    padding: {
+                                        left: 0,
+                                        right: 0,
+                                        top: 0,
+                                        bottom: 0
+                                    }
+                                }
+                            }}
+                        />
+                    </div>
+                </div>
+                ) : (<div>
                     <br>
                     </br>
-                    <h3 style={{color: "chartreuse"}}>{displaymessage}</h3>
-                </div>) }
-            
+                    <h3 style={{ color: "chartreuse" }}>{displaymessage}</h3>
+                </div>)}
+
             </div>
-         );
+        );
     }
 }
- 
+
 export default Dashboard;
